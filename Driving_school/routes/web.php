888a9bafb8e2eb59e00f88143f3c66b4fb558029
@onesimeahtonesimeaht <onesime.ahoton@epitech.eu>
2025-06-20 03:18:18 +0200

    Adding of lessons routes and page

--- Driving_school/routes/web.php
+++ Driving_school/routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cours', function () {
+    return view('cours');
+});
 Route::get('/quiz', function () {
     return view('quiz');
 });
